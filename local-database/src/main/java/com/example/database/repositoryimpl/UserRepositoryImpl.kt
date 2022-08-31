a5cdb5e9b3acec5921326c9567aaebc130e93d61
유현명 <aahspringaa4@naver.com>
2022-08-31 19:16:18 +0900

    ⚡️ :: UserRepositoryImpl

--- local-database/src/main/java/com/example/database/repositoryimpl/UserRepositoryImpl.kt
+++ local-database/src/main/java/com/example/database/repositoryimpl/UserRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.example.database.repositoryimpl
 
-class UserRepositoryImpl {
+import com.example.database.datasource.LocalUserDataSource
+import com.example.database.param.LocalUserViewParam
+import com.example.database.storage.AuthDataStorage
+import com.example.database_domain.entity.UserVisibleEntity
+import com.example.database_domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val authDataStorage: AuthDataStorage
+): UserRepository {
+
+    override suspend fun fetchViewBoolean(): UserVisibleEntity = authDataStorage.fetchViewBoolean()
+
 }
