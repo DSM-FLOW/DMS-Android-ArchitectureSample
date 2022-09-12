efbd29a0291027009a74ed6e98d1640d248b3c2e
유현명 <aahspringaa4@naver.com>
2022-09-13 08:57:04 +0900

    ⚡️ :: SignIn ViewModel Funtion

--- presentation/src/main/java/com/example/dmsarchitecturesample/feature/auth/signin/SignInViewModel.kt
+++ presentation/src/main/java/com/example/dmsarchitecturesample/feature/auth/signin/SignInViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.dmsarchitecturesample.feature.auth.signin
 
+import androidx.lifecycle.viewModelScope
 import com.example.database_domain.usecase.LocalUserViewBooleanUseCase
 import com.example.dmsarchitecturesample.base.BaseViewModel
+import com.example.domain_auth.exception.BadRequestException
+import com.example.domain_auth.exception.ConflictException
+import com.example.domain_auth.exception.NotFoundException
 import com.example.domain_auth.param.LoginParam
 import com.example.domain_auth.usecase.RemoteSignInUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,7 +21,7 @@ class SignInViewModel @Inject constructor(
     private val localUserViewBooleanUseCase: LocalUserViewBooleanUseCase
 ): BaseViewModel<SignInState, SignInEvent>() {
 
-    val parameter =
+    private val parameter =
         LoginParam(id = state.value.id, password = state.value.password)
 
     fun setId(id: String) {
@@ -26,6 +34,24 @@ class SignInViewModel @Inject constructor(
 
     override val initialState: SignInState
         get() = SignInState.initial()
+
+    fun signIn() {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                withContext(Dispatchers.Default) {
+                    remoteSignInUseCase.execute(parameter)
+                }
+            }.onSuccess{
+                SignInEvent.SignInSuccess
+            }.onFailure {
+                when(it) {
+                    is BadRequestException -> SignInEvent.BadRequestException
+                    is NotFoundException -> SignInEvent.NotFoundException
+                    is ConflictException -> SignInEvent.ConflictException
+                }
+            }
+        }
+    }
 
     override fun reduceEvent(oldState: SignInState, event: SignInEvent) {
         when(event) {
